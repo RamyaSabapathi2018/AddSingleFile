72db4f7cc83ad8fc3230aa83a98492a8c5554501
RamyaSabapathi2018 <mailramya26@gmail.com>
2020-09-03 21:27:14 +0530

    Uploaded single folder

---
+++ src/test/java/org/AddOneFile/PushFileOneFile.java
@@ -0,0 +1,8 @@
+package org.AddOneFile;
+
+public class PushFileOneFile {
+	public static void main(String[] args) {
+		System.out.println("Adding PushFileOne file");
+	}
+
+}
---
+++ src/test/java/org/AddOneFile/PushFileTwoFile.java
@@ -0,0 +1,8 @@
+package org.AddOneFile;
+
+public class PushFileTwoFile {
+	public static void main(String[] args) {
+		System.out.println("Adding PushFileTwo file");
+	}
+
+}
